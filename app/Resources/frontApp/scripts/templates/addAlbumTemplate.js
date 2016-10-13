2212ca0e83a323195e29f9c8d2142b145abd15fe
Konstantinos Kataras <konstantinos@kursez.com>
2016-10-13 14:30:52 +0200

    Fixed placeholder value

--- app/Resources/frontApp/scripts/templates/addAlbumTemplate.js
+++ app/Resources/frontApp/scripts/templates/addAlbumTemplate.js
@@ -2,7 +2,7 @@ export default `
 <h2 class="type type--title-md type--gray">Add Album</h2>
 <form class="form">
   <div class="form-field">
-    <input name="create_album[name]" type="text" placeholder="Image name">
+    <input name="create_album[name]" type="text" placeholder="Album name">
   </div>
   <div class="form-field">
     <input name="submit" type="submit" value="submit">
